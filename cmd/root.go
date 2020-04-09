da380649a375cfebf91e0f9850ddf8b8d35674a4
latiif <latiif@mail.com>
2020-04-09 21:14:58 +0200

    Implement logic for DNS Lookup

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,18 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"os"
+	"time"
 
-import "fmt"
+	"github.com/latiif/awaitrmq/pkg/dnslookup"
+	"github.com/spf13/cobra"
+)
 
 var (
-	timeoutFlag   float64
-	intervalFlag  float64
+	// flags
+	timeoutFlag   int64
+	intervalFlag  int64
 	dnslookupFlag bool
 )
 
@@ -15,7 +21,10 @@ var rootCmd = &cobra.Command{
 	Short: "Awaits for a RabbitMQ Service",
 	Long:  "Smartly awaits for a RabbitMQ Service to actually be running",
 	Run: func(cmd *cobra.Command, args []string) {
-		doAwait()
+		if len(args) != 1 {
+			log.Fatalf("%v is not valid. Exactly 1 argument is expected.\n", args)
+		}
+		doAwait(args[0])
 	},
 }
 
@@ -25,12 +34,55 @@ func Execute() error {
 }
 
 func init() {
-	rootCmd.PersistentFlags().Float64VarP(&timeoutFlag, "timeout", "t", 0, "Timeout to stop waiting in milliseconds. Pass 0 to never timeout.")
-	rootCmd.PersistentFlags().Float64VarP(&intervalFlag, "interval", "i", 2000, "Interval between attempts to check in milliseconds")
+	rootCmd.PersistentFlags().Int64VarP(&timeoutFlag, "timeout", "t", 0, "Timeout to stop waiting in milliseconds. Pass 0 to timeout in ~ 290 years.")
+	rootCmd.PersistentFlags().Int64VarP(&intervalFlag, "interval", "i", 2000, "Interval between attempts to check in milliseconds")
 	rootCmd.PersistentFlags().BoolVarP(&dnslookupFlag, "dnslookup", "l", true, "When true, awaits a succesful dnslookup before proceeding.")
 }
 
-func doAwait() {
-	//TODO Implement logic
-	fmt.Println("TODO")
+func doAwait(target string) {
+	if timeoutFlag < 0 {
+		log.Fatalf("value of timeout (%d) is negative.\n", timeoutFlag)
+	}
+	if intervalFlag < 0 {
+		log.Fatalf("value of interval (%d) is negative.\n", intervalFlag)
+	}
+
+	if intervalFlag < 1000 {
+		log.Printf("value of interval (%d) is too low. Consider increasing it above 1000ms", intervalFlag)
+	}
+
+	intervalTicker := time.NewTicker(time.Duration(intervalFlag) * time.Millisecond)
+	var timeoutTimer *time.Timer
+	if timeoutFlag == 0 {
+		log.Printf("timeout equals 0. It will time out in ~290 years.")
+		// Maximum duration roughly 290 years
+		timeoutTimer = time.NewTimer(time.Duration(1<<63 - 1))
+	} else {
+		timeoutTimer = time.NewTimer(time.Duration(timeoutFlag) * time.Millisecond)
+	}
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-timeoutTimer.C:
+				log.Fatalf("Timed out after %d ms.\n", timeoutFlag)
+				done <- false
+				return
+			case <-intervalTicker.C:
+				log.Printf("Attempting to find %s...", target)
+				found := dnslookup.DNSLookup(target)
+				if found {
+					done <- true
+					return
+				}
+			}
+		}
+	}()
+	succesful := <-done
+	if succesful {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
 }
