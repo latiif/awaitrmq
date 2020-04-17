b00e6ccff41c4b23390a9c7f26ead879c78aec2a
Latiif <abdullatif.alshriaf@combitech.se>
2020-04-17 19:55:43 +0200

    Extract hostname from url ignoring port number when doing a dns lookup

--- pkg/dnslookup/dnslookup.go
+++ pkg/dnslookup/dnslookup.go
@@ -2,11 +2,14 @@ package dnslookup
 
 import (
 	"net"
+	"strings"
 )
 
 // DNSLookup returns the result of a dns lookup on the given url
 func DNSLookup(url string) bool {
-	ip, err := net.LookupIP(url)
+	//TODO find a better way to extract host
+	host := strings.Split(url, ":")[0] //remove port number
+	ip, err := net.LookupIP(host)
 	if err == nil && len(ip) != 0 {
 		return true
 	}
